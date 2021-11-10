feae83bda879534255ad9c4290c9d7a103d0f1cc
李云飞 <liyunfei02@daojia-inc.com>
2021-11-10 15:25:00 +0800

    feat: static

--- src/index.ts
+++ src/index.ts
@@ -4,72 +4,19 @@ const path = require("path");
 const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
 const Koa = require('koa')
+const koaStatic = require('koa-static')
 
 const app = new Koa();
 app.use(bodyParser())
-app.use(async (ctx:DefaultContext) => {
-  // let url = ctx.url;
-  // let request = ctx.request;
-  // let req_query = request.query;
-  // let req_querystring = request.querystring;
-  // ctx.body = {
-  //   url,
-  //   req_query,
-  //   req_querystring,
-  // }
-  if ( ctx.url === '/' && ctx.method === 'GET' ) {
-    // 当GET请求时候返回表单页面
-    let html = `
-      <h1>koa2 request post demo</h1>
-      <form method="POST" action="/">
-        <p>userName</p>
-        <input name="userName" /><br/>
-        <p>nickName</p>
-        <input name="nickName" /><br/>
-        <p>email</p>
-        <input name="email" /><br/>
-        <button type="submit">submit</button>
-      </form>
-    `
-    ctx.body = html
-  } else if ( ctx.url === '/' && ctx.method === 'POST' ) {
-    // 当POST请求的时候，解析POST表单里的数据，并显示出来
-    // let postData = await parsePostData( ctx )
-    let postData = ctx.request.body
+// 静态资源目录对于相对入口文件index.js的路径
+const staticPath = './static'
 
-    ctx.body = postData
-  } else {
-    // 其他请求显示404
-    ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
-  }
+app.use(koaStatic(
+  path.join( __dirname,  staticPath)
+))
+app.use( async ( ctx:DefaultContext ) => {
+  ctx.body = 'hello world'
 })
-function parsePostData(ctx:DefaultContext) {
-  return new Promise((resolve, reject) => {
-    try { 
-      let postdata = "";
-      ctx.req.addListener('data', (data: string) => {
-        postdata += data
-      })
-      ctx.req.addListener('end', () =>{
-        let parseData = parseQueryStr(postdata)
-        resolve(parseData)
-      })
-    } catch (err) {
 
-    }
-  })
-}
-function parseQueryStr(queryStr: string) {
-  let queryData: {
-    [x:string]:string
-  } = {}
-  let queryStrList = queryStr.split('&');
-  console.log(queryStrList)
-  for(let [index, queryStr] of queryStrList.entries()) {
-    let itemList = queryStr.split('=');
-    queryData[itemList[0]] = decodeURIComponent(itemList[1])
-  }
-  return queryData
-}
 app.listen(3000)
 console.log('starting at port 3000')
