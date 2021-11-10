2883732dd1c971543ada60e26e90120b5efcb8c3
李云飞 <liyunfei02@daojia-inc.com>
2021-11-10 14:44:37 +0800

    feat: post请求

--- src/index.ts
+++ src/index.ts
@@ -2,56 +2,74 @@ import fs from "fs";
 import { DefaultContext, Next } from "koa";
 const path = require("path");
 const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
 const Koa = require('koa')
 
 const app = new Koa();
-// const router = new Router();
-// router.get('/', (ctx: DefaultContext, next: Next) => {
-//   console.log(111)
-//   ctx.body = 'hello world'
-// }).get('/user', (ctx:DefaultContext, next:Next) => {
-//   ctx.body = 'user'
-// }).get('/user/:id', (ctx:DefaultContext, next:Next) => {
-//   console.log(ctx._matchedRoute)
-//   ctx.body = '1'
-// })
-// router.get('user', '/users/:id', (ctx:DefaultContext, next:Next) => {
-//   console.log('sss')
-//   ctx.body = '2'
-//  });
-//  router.get('/users:id', (ctx:DefaultContext, next:Next) => {
-//    return User.findById(ctx.params.id).then(user => {
-//      ctx.user = user;
-//      next()
-//    })
-//  })
-// router.url('user', 3);
+app.use(bodyParser())
+app.use(async (ctx:DefaultContext) => {
+  // let url = ctx.url;
+  // let request = ctx.request;
+  // let req_query = request.query;
+  // let req_querystring = request.querystring;
+  // ctx.body = {
+  //   url,
+  //   req_query,
+  //   req_querystring,
+  // }
+  if ( ctx.url === '/' && ctx.method === 'GET' ) {
+    // 当GET请求时候返回表单页面
+    let html = `
+      <h1>koa2 request post demo</h1>
+      <form method="POST" action="/">
+        <p>userName</p>
+        <input name="userName" /><br/>
+        <p>nickName</p>
+        <input name="nickName" /><br/>
+        <p>email</p>
+        <input name="email" /><br/>
+        <button type="submit">submit</button>
+      </form>
+    `
+    ctx.body = html
+  } else if ( ctx.url === '/' && ctx.method === 'POST' ) {
+    // 当POST请求的时候，解析POST表单里的数据，并显示出来
+    // let postData = await parsePostData( ctx )
+    let postData = ctx.request.body
 
-// var forums = new Router();
-// var posts = new Router();
-// posts.get('/', (ctx:DefaultContext, next:Next) => {})
-// posts.get('/:pid', (ctx:DefaultContext, next:Next) => {})
-// console.log(posts.routes())
-// forums.use('/forums/:fid/posts', posts.routes(), posts.allowedMethods())
+    ctx.body = postData
+  } else {
+    // 其他请求显示404
+    ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
+  }
+})
+function parsePostData(ctx:DefaultContext) {
+  return new Promise((resolve, reject) => {
+    try { 
+      let postdata = "";
+      ctx.req.addListener('data', (data: string) => {
+        postdata += data
+      })
+      ctx.req.addListener('end', () =>{
+        let parseData = parseQueryStr(postdata)
+        resolve(parseData)
+      })
+    } catch (err) {
 
-// app.use(router.routes())
-//   .use(router.allowedMethods())
-let home = new Router();
-home.get('/b', async (ctx:DefaultContext, next:Next) =>{
-  let html = 'hello word'
-  ctx.body = html
-})
-let page = new Router();
-page.get('/', async (ctx:DefaultContext, next:Next) =>{
-  ctx.body = '404 page'
-}).get('/helloword', async (ctx:DefaultContext, next:Next) =>{
-  ctx.body = 'hello page'
-})
-let router = new Router();
-router.redirect('/a/b', '/page/helloword')
-router.use('/a', home.routes(), home.allowedMethods());
-router.use('/page', page.routes(), page.allowedMethods())
-app.use(router.routes())
-    .use(router.allowedMethods())
+    }
+  })
+}
+function parseQueryStr(queryStr: string) {
+  let queryData: {
+    [x:string]:string
+  } = {}
+  let queryStrList = queryStr.split('&');
+  console.log(queryStrList)
+  for(let [index, queryStr] of queryStrList.entries()) {
+    let itemList = queryStr.split('=');
+    queryData[itemList[0]] = decodeURIComponent(itemList[1])
+  }
+  return queryData
+}
 app.listen(3000)
 console.log('starting at port 3000')
