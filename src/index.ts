5317b35d502a53d31110f21247d5129eabc3a073
李云飞 <liyunfei02@daojia-inc.com>
2022-04-12 17:40:40 +0800

    feat: post请求

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,22 @@ console.log('hello word!')
 import Koa from 'koa'
 import Router from '@koa/router'
 import logger from './logger'
+import bodyParser from 'koa-bodyparser'
 const app = new Koa ();
 const router = new Router();
+app.use(bodyParser())
 router
-  .get('/', async (ctx) => {
-    ctx.body = 'hello world'
-  })
   .get('/user', async (ctx) => {
-    ctx.body = 'user'
+    const query = ctx.query;
+    ctx.body = query
+  })
+  .post('/user', async (ctx) => {
+    console.log(ctx.request.body)
+    ctx.body = ctx.request.body;
   })
 app
   .use(router.routes())
   .use(router.allowedMethods())
   .use(logger())
 
-app.listen(3000)
+app.listen(3003)
